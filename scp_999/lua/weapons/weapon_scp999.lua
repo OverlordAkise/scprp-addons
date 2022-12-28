c28a83e97ec2c0c65aa44766166be887ccee6d4d
OverlordAkise <OverlordAkise@gmail.com>
2022-12-28 23:18:10 +0100

    Added SCP999 particle effect

--- scp_999/lua/weapons/weapon_scp999.lua
+++ scp_999/lua/weapons/weapon_scp999.lua
@@ -1,5 +1,7 @@
 --THIS FILE IS MADE BY BananowyTasiemiec
---THIS FILE IS MADE BY BananowyTasiemiec
+--Edited by OverlordAkise
+
+--Added: Particle Effect on heal
 
 SWEP.Primary.ClipSize = -1
 SWEP.Primary.DefaultClip = -1
@@ -64,9 +66,11 @@ function SWEP:PrimaryAttack()
 			else
 				ply:SetHealth(ply:GetMaxHealth())
 			end
+            self:EmitParticles(ply)
 			if ( CLIENT ) then return end
 			self.Owner:EmitSound("scp999/scp999_"..math.random(1,6)..".mp3", 75)
 			self.Weapon:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+            
 		end
 	end
 end
@@ -89,3 +93,25 @@ hook.Add( "EntityTakeDamage", "EntityDamageExample", function( target, dmginfo )
 		end
 	end
 end )
+
+function SWEP:EmitParticles(ply)
+    if SERVER then return end
+    chat.AddText("SWEP:EmitParticles")
+    local pos = ply:GetPos()+Vector(0,0,40)
+    local emitter = ParticleEmitter(pos, false)
+    for i=0,200 do
+        chat.AddText(i)
+        local mat = Material("icon16/heart.png")
+        local particle = emitter:Add( mat, pos + Vector(math.random(-10, 10), math.random(-10, 10), math.random(-10, 10)))
+        if particle then
+            particle:SetDieTime( 2 )
+            particle:SetStartAlpha( 100 )
+            particle:SetEndAlpha( 0 )
+            particle:SetStartSize( 1 )
+            particle:SetEndSize( 1 )
+            particle:SetColor(255, 0, 0)
+            particle:SetGravity(Vector(math.Rand(-10, 10), math.Rand(-10, 10), math.Rand(-20, 20)))
+        end
+    end
+	emitter:Finish()
+end
