1bb30f7399f8c4fa4a81b1f91ff27d7476a83c43
OverlordAkise <OverlordAkise@gmail.com>
2022-12-29 21:04:06 +0100

    Added GuthSCP Keycard Animations
    These can be simply drag&drop'd in the addons folder and it will enhance GuthSCPs keycards.

---
+++ luctus_keycard_anim/lua/autorun/client/cl_luctus_keyanim.lua
@@ -0,0 +1,34 @@
+--Luctus Keycard Animation
+--Made by OverlordAkise
+
+local endRange = 0
+local animationTime = 0.2
+local curwep = nil
+
+net.Receive("guthscp_animation",function()
+    --if not LocalPlayer():GetActiveWeapon().GuthSCPLVL then return end
+    local ply = LocalPlayer()
+    local start = SysTime()
+    if net.ReadBool() then
+        if ply:GetActiveWeapon().GuthSCPLVL then
+            curwep = ply:GetActiveWeapon()
+        end
+        timer.Create("guthscp_animation",0,100,function()
+            if not ply:GetActiveWeapon().GuthSCPLVL then return end
+            ply:GetActiveWeapon().ViewModelFOV = Lerp( (SysTime() - start)/animationTime, 55, 100 )
+            endRange = Lerp( (SysTime() - start)/animationTime, 55, 100 )
+        end)
+    else
+        if not ply:GetActiveWeapon().GuthSCPLVL then
+            curwep.ViewModelFOV = 55
+            return
+        end
+        timer.Create("guthscp_animation",0,100,function()
+            if not ply:GetActiveWeapon().GuthSCPLVL then return end
+            ply:GetActiveWeapon().ViewModelFOV = Lerp( (SysTime() - start)/animationTime, endRange, 55 )
+        end)
+    end
+end)
+
+print("[luctus_keycard_animation] CL loaded!")
+
---
+++ luctus_keycard_anim/lua/autorun/server/sv_luctus_keyanim.lua
@@ -0,0 +1,35 @@
+--Luctus Keycard Animation
+--Made by OverlordAkise
+
+util.AddNetworkString("guthscp_animation")
+
+hook.Add("PlayerUse", "luctus_guthscp_animation", function(ply, ent)
+    --Copied from GuthSCP
+    if not IsValid(ent) or not IsValid(ply) then return end
+    if not GuthSCP.keycardAvailableClass[ent:GetClass()] then return end
+    --print(ply.guthscp_last_use_time,CurTime() + GuthSCP.useCooldown)
+    --Own:
+    if not ply:GetActiveWeapon().GuthSCPLVL then return end
+    if ply.already_animating then return end
+    ply.already_animating = true
+    local curwep = ply:GetActiveWeapon()
+    net.Start("guthscp_animation")
+        net.WriteBool(true)
+    net.Send(ply)
+    curwep:SetHoldType("pistol")
+    timer.Simple(0.3,function()
+        if not IsValid(ply) then return end
+        if not curwep then return end
+        net.Start("guthscp_animation")
+            net.WriteBool(false)
+        net.Send(ply)
+        curwep:SetHoldType("slam")
+        timer.Simple(0.2,function()
+            if not IsValid(ply) then return end
+            ply.already_animating = false
+        end)
+    end)
+end)
+
+print("[luctus_keycard_animation] SV loaded!")
+
