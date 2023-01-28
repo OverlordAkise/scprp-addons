aa94b2e3a0eca5c773ae8d33e24a2a657aa708f1
OverlordAkise <OverlordAkise@gmail.com>
2023-01-28 20:22:26 +0100

    Code format and more prefixes

--- luctus_funkcmd/lua/autorun/server/sv_luctus_funk.lua
+++ luctus_funkcmd/lua/autorun/server/sv_luctus_funk.lua
@@ -2,26 +2,26 @@
 --Made by OverlordAkise
 
 function luctusEncrypt(text)
-  textArray = string.Explode("", string.lower(text))
-  for k, v in pairs(textArray) do
-    if LUCTUS_FUNK_ENCTABLE[v] then
-      textArray[k] = LUCTUS_FUNK_ENCTABLE[v]
+    textArray = string.Explode("", string.lower(text))
+    for k, v in pairs(textArray) do
+        if LUCTUS_FUNK_ENCTABLE[v] then
+            textArray[k] = LUCTUS_FUNK_ENCTABLE[v]
+        end
     end
-  end
-  return string.Implode("", textArray)
+    return string.Implode("", textArray)
 end
 
 function luctusDecrypt(text)
-  textArray = string.Explode("", string.lower(text))
-  for k, v in pairs(textArray) do
-    for kk,vv in pairs(LUCTUS_FUNK_ENCTABLE) do
-      if vv == v then
-        textArray[k] = kk
-        break
-      end
+    textArray = string.Explode("", string.lower(text))
+    for k, v in pairs(textArray) do
+        for kk,vv in pairs(LUCTUS_FUNK_ENCTABLE) do
+            if vv == v then
+                textArray[k] = kk
+            break
+            end
+        end
     end
-  end
-  return string.Implode("", textArray)
+    return string.Implode("", textArray)
 end
 
 util.AddNetworkString("luctus_funk_broadcast")
@@ -31,48 +31,47 @@ util.AddNetworkString("luctus_funk_opendec")
 util.AddNetworkString("luctus_funk_decrypt")
 
 hook.Add("PlayerSay", "comms.cmdplysay", function( ply, text )
-  if string.StartWith(string.lower(text), LUCTUS_FUNK_COMMAND) then
-    local contents = string.Explode(" ", text)
-    local receivent = contents[2]
-    local msgcontent = string.sub(text, #contents[1] + #contents[2] + 2, nil)
-    net.Start( "luctus_funk_broadcast" )
-      net.WriteEntity(ply)
-      net.WriteString(receivent)
-      net.WriteString(msgcontent)
-    net.Broadcast()
-    return ""
-  end
-  if string.StartWith(string.lower(text), LUCTUS_FUNK_COMMAND_ANON) then
-    local acontents = string.Explode(" ", text)
-    local areceivent = acontents[2]
-    local amsgcontent = string.sub(text, #acontents[1] + #acontents[2] + 2, nil)
-    net.Start("luctus_funk_anon")
-      net.WriteString(" Anonymous: *" .. areceivent .. "* " .. amsgcontent)
-    net.Broadcast()
-    return ""
-  end
-  if string.StartWith(string.lower(text), LUCTUS_FUNK_COMMAND_ENCRYPTED) then
-    local vcontents = string.Explode(" ", text)
-    local vreceivent = vcontents[2]
-    local vmsgcontent = string.sub(text, #vcontents[1] + #vcontents[2] + 2, nil)
-    net.Start( "luctus_funk_anon_enc" )
-      net.WriteString(" Anonymous: *" .. vreceivent .. "* " .. luctusEncrypt(vmsgcontent))
-    net.Broadcast()
-    return ""
-  end
+    if string.StartWith(string.lower(text), LUCTUS_FUNK_COMMAND) then
+        local contents = string.Explode(" ", text)
+        local receivent = contents[2]
+        local msgcontent = string.sub(text, #contents[1] + #contents[2] + 2, nil)
+        net.Start( "luctus_funk_broadcast" )
+            net.WriteEntity(ply)
+            net.WriteString(receivent)
+            net.WriteString(msgcontent)
+        net.Broadcast()
+        return ""
+    end
+    if string.StartWith(string.lower(text), LUCTUS_FUNK_COMMAND_ANON) then
+        local acontents = string.Explode(" ", text)
+        local areceivent = acontents[2]
+        local amsgcontent = string.sub(text, #acontents[1] + #acontents[2] + 2, nil)
+        net.Start("luctus_funk_anon")
+            net.WriteString(" Anonymous: *" .. areceivent .. "* " .. amsgcontent)
+        net.Broadcast()
+        return ""
+    end
+    if string.StartWith(string.lower(text), LUCTUS_FUNK_COMMAND_ENCRYPTED) then
+        local vcontents = string.Explode(" ", text)
+        local vreceivent = vcontents[2]
+        local vmsgcontent = string.sub(text, #vcontents[1] + #vcontents[2] + 2, nil)
+        net.Start( "luctus_funk_anon_enc" )
+            net.WriteString(" Anonymous: *" .. vreceivent .. "* " .. luctusEncrypt(vmsgcontent))
+        net.Broadcast()
+        return ""
+    end
 end)
 
 net.Receive("luctus_funk_decrypt",function(len,ply)
-  local terminal = net.ReadEntity()
-  if not terminal or not IsValid(terminal) then return end
-  if terminal:GetClass() ~= "luctus_funk_decryptor" then return end
-  if ply:GetPos():Distance(terminal:GetPos()) > 500 then
-    DarkRP.notify(ply,1,5,"You are too far away from the decryptor!")
-    return
-  end
-  local text = net.ReadString()
-  net.Start("luctus_funk_decrypt")
-    net.WriteString(luctusDecrypt(text))
-  net.Send(ply)
+    local terminal = net.ReadEntity()
+    if not terminal or not IsValid(terminal) then return end
+    if terminal:GetClass() ~= "luctus_funk_decryptor" then return end
+    if ply:GetPos():Distance(terminal:GetPos()) > 500 then
+        DarkRP.notify(ply,1,5,"You are too far away from the decryptor!")
+        return
+    end
+    local text = net.ReadString()
+    net.Start("luctus_funk_decrypt")
+        net.WriteString(luctusDecrypt(text))
+    net.Send(ply)
 end)
-
--- luctus_funkcmd/lua/autorun/sh_luctus_funk_config.lua
+++ luctus_funkcmd/lua/autorun/sh_luctus_funk_config.lua
@@ -4,7 +4,9 @@
 LUCTUS_FUNK_COMMAND = "!funk"
 LUCTUS_FUNK_COMMAND_ANON = "!funka"
 LUCTUS_FUNK_COMMAND_ENCRYPTED = "!funke"
-LUCTUS_FUNK_PREFIX = "[FUNK]"
+LUCTUS_FUNK_PREFIX = "[FUNK BROADCAST]"
+LUCTUS_FUNKA_PREFIX = "[FUNK ANON]"
+LUCTUS_FUNKE_PREFIX = "[FUNK ENCRYPTED]"
 
 --Users could decrypt messages automatically
 --if you don't change the encryption table often
@@ -12,42 +14,42 @@ LUCTUS_FUNK_PREFIX = "[FUNK]"
 LUCTUS_FUNK_RANDOM_ENCRYPTION = false
 
 LUCTUS_FUNK_ENCTABLE = {
-  ["a"] = "i",
-  ["b"] = "q",
-  ["c"] = "m",
-  ["d"] = "z",
-  ["e"] = "f",
-  ["f"] = "j",
-  ["g"] = "o",
-  ["h"] = "v",
-  ["i"] = "s",
-  ["j"] = "p",
-  ["k"] = "y",
-  ["l"] = "x",
-  ["m"] = "d",
-  ["n"] = "a",
-  ["o"] = "h",
-  ["p"] = "c",
-  ["q"] = "e",
-  ["r"] = "k",
-  ["s"] = "t",
-  ["t"] = "w",
-  ["u"] = "r",
-  ["v"] = "n",
-  ["w"] = "g",
-  ["x"] = "b",
-  ["y"] = "l",
-  ["z"] = "u"
+    ["a"] = "i",
+    ["b"] = "q",
+    ["c"] = "m",
+    ["d"] = "z",
+    ["e"] = "f",
+    ["f"] = "j",
+    ["g"] = "o",
+    ["h"] = "v",
+    ["i"] = "s",
+    ["j"] = "p",
+    ["k"] = "y",
+    ["l"] = "x",
+    ["m"] = "d",
+    ["n"] = "a",
+    ["o"] = "h",
+    ["p"] = "c",
+    ["q"] = "e",
+    ["r"] = "k",
+    ["s"] = "t",
+    ["t"] = "w",
+    ["u"] = "r",
+    ["v"] = "n",
+    ["w"] = "g",
+    ["x"] = "b",
+    ["y"] = "l",
+    ["z"] = "u"
 }
 
 --Config End
 
 if LUCTUS_FUNK_RANDOM_ENCRYPTION then
-  local alpha = "abcdefghijklmnopqrstuvwxyz"
-  for k,v in pairs(LUCTUS_FUNK_ENCTABLE) do
-    local randomCharNum = math.random(1,#alpha)
-    local randomChar = string.sub(alpha,randomCharNum,randomCharNum)
-    LUCTUS_FUNK_ENCTABLE[k] = randomChar
-    alpha = string.gsub(alpha,randomChar,"")
-  end
+    local alpha = "abcdefghijklmnopqrstuvwxyz"
+    for k,v in pairs(LUCTUS_FUNK_ENCTABLE) do
+        local randomCharNum = math.random(1,#alpha)
+        local randomChar = string.sub(alpha,randomCharNum,randomCharNum)
+        LUCTUS_FUNK_ENCTABLE[k] = randomChar
+        alpha = string.gsub(alpha,randomChar,"")
+    end
 end
