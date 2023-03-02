142fe6f9d0388141f405db29da7d299a55767c71
OverlordAkise <OverlordAkise@gmail.com>
2023-03-02 22:41:05 +0100

    Added "approve-only" breach
    Added a setting so that a team member has to approve your breach after the timer with a random 4-digit id.

--- luctus_breach/lua/autorun/server/sv_breach.lua
+++ luctus_breach/lua/autorun/server/sv_breach.lua
@@ -3,18 +3,23 @@
 
 
 --In seconds, after joining job when you are allowed to breach
-BREACH_DELAY = 10
-
+LUCTUS_BREACH_DELAY = 300
+--If a teammember has to approve your breach
+LUCTUS_BREACH_NEEDS_APPROVAL = true
+--Usergroups who can approve a breach
+LUCTUS_BREACH_APPROVER = {
+    ["superadmin"] = true,
+    ["admin"] = true,
+    ["operator"] = true,
+    ["moderator"] = true,
+    ["supporter"] = true,
+}
 
 --Jobnames and the buttons that the breachsystem will press if a breach is happening for that job
-
 --The button list consists of either a string name or number ID, example:
-
 -- ["SCP173"] = {"button_name",4100},
-
 --Get the name of a button with console command (ingame) 'breach_get_button'
-
-BREACH_ENABLED_JOBS = {
+LUCTUS_BREACH_JOBS = {
     ["Hobo"] = {4100,"LCZ_door11button"},
 }
 
@@ -28,31 +33,54 @@ end
 
 hook.Add("OnPlayerChangedTeam", "BreachLogic", function(ply, beforejob, afterjob)
     ply.canbreach = false
-    if BREACH_ENABLED_JOBS[team.GetName(afterjob)] then
-        NotifyPlayer(ply,"[breach] You are allowed to breach in "..BREACH_DELAY.." seconds!")
+    ply.breachid = nil
+    if LUCTUS_BREACH_JOBS[team.GetName(afterjob)] then
+        NotifyPlayer(ply,"[breach] You are allowed to breach in "..LUCTUS_BREACH_DELAY.." seconds!")
         CreateBreachTimer(ply)
     else
         timer.Remove(ply:SteamID64().."_breachtimer")
     end
 end)
 
+function LuctusBreachDo(ply)
+    NotifyPlayer(ply,"[breach] Opening doors for you...")
+    ply.canbreach = false
+    CreateBreachTimer(ply)
+    for k,v in pairs(LUCTUS_BREACH_JOBS[ply:getJobTable().name]) do
+        if type(v) == "string" then
+            if ents.FindByName(v)[1] and IsValid(ents.FindByName(v)[1]) then
+                ents.FindByName(v)[1]:Use(Entity(0)) --entity0=worldspawn
+            end
+        else
+            if IsValid(ents.GetMapCreatedEntity(v)) then
+                ents.GetMapCreatedEntity(v):Use(Entity(0))
+            end
+        end
+    end
+end
+
+function LuctusBreachSeekApproval(breacher)
+    for k,v in pairs(player.GetAll()) do
+        if not LUCTUS_BREACH_APPROVER[v:GetUserGroup()] then continue end
+        NotifyPlayer(v,"[breach] '"..breacher:Nick().."' wants to breach!")
+        NotifyPlayer(v,"[breach] Type '!approve "..breacher.breachid.."' to approve this breach!")
+    end
+end
+
 hook.Add("PlayerSay", "BreachCommand", function(ply, text)
     if text == "!breach" then
-        if BREACH_ENABLED_JOBS[ply:getJobTable().name] then
+        if LUCTUS_BREACH_JOBS[ply:getJobTable().name] then
             if ply.canbreach == true then
-                NotifyPlayer(ply,"[breach] Opening doors for you...")
-                ply.canbreach = false
-                CreateBreachTimer(ply)
-                for k,v in pairs(BREACH_ENABLED_JOBS[ply:getJobTable().name]) do
-                    if type(v) == "string" then
-                        if ents.FindByName(v)[1] and IsValid(ents.FindByName(v)[1]) then
-                            ents.FindByName(v)[1]:Use(Entity(0)) --entity0=worldspawn
-                        end
-                    else
-                        if IsValid(ents.GetMapCreatedEntity(v)) then
-                            ents.GetMapCreatedEntity(v):Use(Entity(0))
-                        end
+                if LUCTUS_BREACH_NEEDS_APPROVAL then
+                    if ply.breachid then
+                        NotifyPlayer(ply,"[breach] You already requested a breach! Your ID: "..ply.breachid)
+                        return
                     end
+                    ply.breachid = ""..math.random(1,9999)
+                    LuctusBreachSeekApproval(ply)
+                    NotifyPlayer(ply,"[breach] Your request to breach has been created! Your ID: "..ply.breachid)
+                else
+                    LuctusBreachDo(ply)
                 end
             else
                 NotifyPlayer(ply,"[breach] You can't breach yet! Time left: "..math.Round(timer.TimeLeft(ply:SteamID64().."_breachtimer")).." seconds.")
@@ -61,11 +89,25 @@ hook.Add("PlayerSay", "BreachCommand", function(ply, text)
             NotifyPlayer(ply,"[breach] Your job doesn't allow breaching!")
         end
     end
+    if LUCTUS_BREACH_APPROVER[ply:GetUserGroup()] and string.StartWith(text,"!approve") then
+        local name = string.Split(text,"!approve ")
+        PrintTable(name)
+        if not name[2] then return end
+        for k,v in pairs(player.GetAll()) do
+            if v.breachid and v.breachid == name[2] then
+                LuctusBreachDo(v)
+                NotifyPlayer(ply,"[breach] Successfully approved!")
+                return
+            end
+        end
+        NotifyPlayer(ply,"[breach] Error, ID not found!")
+    end
 end)
 
 function CreateBreachTimer(ply)
     ply.canbreach = false
-    timer.Create(ply:SteamID64().."_breachtimer", BREACH_DELAY, 1, function()
+    ply.breachid = nil
+    timer.Create(ply:SteamID64().."_breachtimer", LUCTUS_BREACH_DELAY, 1, function()
         ply.canbreach = true
         NotifyPlayer(ply,"[breach] You are now allowed to breach! Type '!breach' to open your doors to freedom.")
     end)
