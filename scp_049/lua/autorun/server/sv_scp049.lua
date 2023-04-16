a99efae423fd3ff9ca7a52af26f2ff81fbffaebc
OverlordAkise <OverlordAkise@gmail.com>
2023-04-16 23:30:32 +0200

    [scp049] Reworked completely
     - Instead of copying the code for the fists simply Base it on it
     - Leftlick now kills players, open doors and revives corpses all at once
     - Removed killaura
     - Reworked logic for spawning zombies

--- scp_049/lua/autorun/server/sv_scp049.lua
+++ scp_049/lua/autorun/server/sv_scp049.lua
@@ -4,144 +4,100 @@
 util.AddNetworkString("luctus_scp049_mixing")
 resource.AddWorkshop("183901628") -- SCP049 Playermodel
 
-scp049_ply = scp049_ply or false
-scp049_col_one = 1
-scp049_col_two = 1
-scp049_random = tonumber(os.date("%W")) --unused
-
-
 scp049_effects = {
-  ["Nothing"] = 60, 
-  ["Jumpheight"] = 20,
-  ["Health"] = 15,
-  ["Damage"] = 10,
-  ["Speed"] = 10,
-  ["Godly"] = 5,
+    ["Nothing"] = 60,
+    ["Jumpheight"] = 20,
+    ["Health"] = 15,
+    ["Damage"] = 10,
+    ["Speed"] = 10,
+    ["Godly"] = 5,
 }
 
 scp049_effect_functions = {
-  ["Nothing"] = function(ply) end, 
-  ["Jumpheight"] = function(ply)
-    local spawnPos = ply:GetPos()
-    ply:Spawn()
-    ply.scp049_oldJob = ply:getDarkRPVar("job")
-    ply:setDarkRPVar("job","SCP049-2")
-    ply:SetPos(spawnPos)
-    ply:StripWeapons()
-    ply:Give("weapon_scp049_2")
-    ply:SetJumpPower(400)
-    ply:SetModel(SCP049_ZOMBIE_MODEL)
-  end,
-  ["Health"] = function(ply)
-    local spawnPos = ply:GetPos()
-    ply:Spawn()
-    ply.scp049_oldJob = ply:getDarkRPVar("job")
-    ply:setDarkRPVar("job","SCP049-2")
-    ply:SetPos(spawnPos)
-    ply:StripWeapons()
-    ply:Give("weapon_scp049_2")
-    ply:SetHealth(1000)
-    ply:SetMaxHealth(1000)
-    ply:SetModel(SCP049_ZOMBIE_MODEL)
-  end,
-  ["Damage"] = function(ply)
-    local spawnPos = ply:GetPos()
-    ply:Spawn()
-    ply.scp049_oldJob = ply:getDarkRPVar("job")
-    ply:setDarkRPVar("job","SCP049-2")
-    ply:SetPos(spawnPos)
-    ply:StripWeapons()
-    ply:Give("weapon_scp049_2")
-    ply:GetWeapons()[1].Primary.Damage = 60
-    ply:SetModel(SCP049_ZOMBIE_MODEL)
-  end,
-  ["Speed"] = function(ply)
-    local spawnPos = ply:GetPos()
-    ply:Spawn()
-    ply.scp049_oldJob = ply:getDarkRPVar("job")
-    ply:setDarkRPVar("job","SCP049-2")
-    ply:SetPos(spawnPos)
-    ply:StripWeapons()
-    ply:Give("weapon_scp049_2")
-    ply:SetRunSpeed(ply:GetRunSpeed()*2)
-    ply:SetWalkSpeed(ply:GetWalkSpeed()*2)
-    ply:SetDuckSpeed(ply:GetWalkSpeed()*2)
-    ply:SetModel(SCP049_ZOMBIE_MODEL)
-  end,
-  ["Godly"] = function(ply)
-    local spawnPos = ply:GetPos()
-    ply:Spawn()
-    ply:SetPos(spawnPos)
-  end,
+    ["Nothing"] = function(ply) end, 
+    ["Jumpheight"] = function(ply)
+        ply:SetJumpPower(400)
+    end,
+    ["Health"] = function(ply)
+        ply:SetHealth(2000)
+        ply:SetMaxHealth(2000)
+    end,
+    ["Damage"] = function(ply)
+        ply:GetWeapons()[1].Primary.Damage = 60
+    end,
+    ["Speed"] = function(ply)
+        ply:SetRunSpeed(ply:GetRunSpeed()*2)
+        ply:SetWalkSpeed(ply:GetWalkSpeed()*2)
+        ply:SetDuckSpeed(ply:GetWalkSpeed()*2)
+    end,
+    ["Godly"] = function(ply)
+        local spawnPos = ply:GetPos()
+        ply:Spawn()
+        ply:SetPos(spawnPos)
+    end,
 }
 
-scp049_mixtable = scp049_mixtable or {}
-
-
-function luctusMixTable()
-  scp049_mixtable = {}
-  local chc = 0
-	local lootChc = {}
-	local lootClass = ""
-    	local loot
-	for k, v in pairs ( scp049_effects ) do
-		lootChc[k] = { min = chc+1, max = chc + v }
-		chc = chc + v
-	end
-  print("[SCP049] The Mixtable is:")
-  for i=1,7 do
-    scp049_mixtable[i] = {}
-    for j=1,7 do
-      local rNumber = math.random( 1, chc )
-      for k, v in pairs ( lootChc ) do
-        if ( rNumber >= v.min and rNumber <= v.max ) then
-          lootClass = k
-          print("["..i.."]["..j.."] = "..k)
-          scp049_mixtable[i][j] = k
-        end
-      end
-    end
-  end
-  print("[SCP049] End of mixtable")
-end
-  
-hook.Add("OnPlayerChangedTeam", "luctus_scp049_killaura", function(ply, beforeNum, afterNum)
-
-  --switch to scp173
-  if RPExtraTeams[afterNum].name == "SCP 049" then
-    luctusMixTable()
-    scp049_ply = ply
-    --PrintMessage(HUD_PRINTTALK, "Someone switched to SCP 049!")
-    timer.Create("scp049_killaura", 0.1, 0, function()
-      for k,v in pairs(ents.FindInSphere( scp049_ply:GetPos(), 100 )) do
-        if v ~= scp049_ply and v:IsPlayer() and not v.scp049_killed and not SCP049_SAVE_PMODELS[v:GetModel()] then
-          v.scp049_killed = true
-          v:Kill()
-        end
-      end
+function LuctusSCP049SpawnZombie(mixedFunc,ply,funcName)
+    local spawnPos = ply:GetPos()
+    ply:Spawn()
+    timer.Simple(0.1,function()--needed for spawnPos
+        if not IsValid(ply) then return end
+        ply:SetPos(spawnPos)
+        if funcName == "Godly" then return end
+        ply.scp049_oldJob = ply:getDarkRPVar("job")
+        ply:setDarkRPVar("job","SCP049-2")
+        ply:StripWeapons()
+        ply:Give("weapon_scp049_2")
+        ply:SetModel(SCP049_ZOMBIE_MODEL)
+        mixedFunc(ply)
     end)
-  end
+end
 
-  --switch from scp173
-  if RPExtraTeams[beforeNum].name == "SCP 049" then
-    scp049_ply = nil
-    --PrintMessage(HUD_PRINTTALK, "Someone switched from SCP 049!")
-    timer.Remove("scp049_killaura")
-  end
+scp049_mixtable = scp049_mixtable or {}
+function luctusMixTable()
+    scp049_mixtable = {}
+    local chc = 0
+    local lootChc = {}
+    local lootClass = ""
+    local loot
+    for k, v in pairs ( scp049_effects ) do
+        lootChc[k] = { min = chc+1, max = chc + v }
+        chc = chc + v
+    end
+    print("[SCP049] The Mixtable is:")
+    for i=1,7 do
+        scp049_mixtable[i] = {}
+        for j=1,7 do
+            local rNumber = math.random( 1, chc )
+            for k, v in pairs ( lootChc ) do
+                if ( rNumber >= v.min and rNumber <= v.max ) then
+                    lootClass = k
+                    print("["..i.."]["..j.."] = "..k)
+                    scp049_mixtable[i][j] = k
+                end
+            end
+        end
+    end
+    print("[SCP049] End of mixtable")
+end
+
+hook.Add("OnPlayerChangedTeam", "luctus_scp049_mix_new", function(ply, beforeNum, afterNum)
+    if string.EndsWith(RPExtraTeams[afterNum].name,"049") then
+        luctusMixTable()
+    end
 end)
 
-hook.Add("PlayerSpawn", "luctus_scp049_antispawnkill", function(ply)
-  ply.scp049_killed = false
-  if ply.scp049_oldJob then
-    ply:setDarkRPVar("job",ply.scp049_oldJob)
-    ply.scp049_oldJob = nil
-  end
+hook.Add("PlayerSpawn", "luctus_scp049_resetjob", function(ply)
+    if ply.scp049_oldJob then
+        ply:setDarkRPVar("job",ply.scp049_oldJob)
+        ply.scp049_oldJob = nil
+    end
 end)
 
 net.Receive("luctus_scp049_mixing",function(len,ply)
-  scp049_col_one = math.Clamp(net.ReadInt(17),1,7)
-  scp049_col_two = math.Clamp(net.ReadInt(17),1,7)
-  ply:PrintMessage(HUD_PRINTTALK, "Your mixed ID is "..scp049_col_one..scp049_col_two)
+    ply.scp049_col_one = math.Clamp(net.ReadInt(17),1,7)
+    ply.scp049_col_two = math.Clamp(net.ReadInt(17),1,7)
+    ply:PrintMessage(HUD_PRINTTALK, "Your mixed ID is "..ply.scp049_col_one..ply.scp049_col_two)
 end)
 
 print("[SCP049] SV Loaded!")
--- scp_049/lua/weapons/weapon_scp049.lua
+++ scp_049/lua/weapons/weapon_scp049.lua
@@ -1,24 +1,19 @@
 AddCSLuaFile()
 
 scp049_colors = {
-  ["Red"] = {1,Color(255,0,0)},
-  ["Green"] = {2,Color(0,255,0)},
-  ["Blue"] = {3,Color(0,0,255)},
-  ["Yellow"] = {4,Color(255,215,0)},
-  ["Purple"] = {5,Color(128,0,128)},
-  ["Cyan"] = {6,Color(0,255,255)},
-  ["Grey"] = {7,Color(211,211,211)},
+    ["Red"] = {1,Color(255,0,0)},
+    ["Green"] = {2,Color(0,255,0)},
+    ["Blue"] = {3,Color(0,0,255)},
+    ["Yellow"] = {4,Color(255,215,0)},
+    ["Purple"] = {5,Color(128,0,128)},
+    ["Cyan"] = {6,Color(0,255,255)},
+    ["Grey"] = {7,Color(211,211,211)},
 }
 
-if CLIENT then
-    SWEP.Slot = 2
-    SWEP.SlotPos = 1
-    SWEP.DrawAmmo = false
-    SWEP.DrawCrosshair = false
-end
-
--- Variables that are used on both client and server
-DEFINE_BASECLASS("weapon_cs_base2")
+SWEP.Slot = 2
+SWEP.SlotPos = 1
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = true
 
 SWEP.PrintName = "SCP049 Weapon"
 SWEP.Author = "OverlordAkise"
@@ -40,7 +35,8 @@ SWEP.Spawnable = true
 SWEP.AdminOnly = false
 SWEP.Category = "SCP"
 
-SWEP.Sound = "physics/wood/wood_box_impact_hard3.wav"
+SWEP.Sound = "ambient/materials/metal_big_impact_scrape1.wav"
+SWEP.KillSound = "player/pl_pain7.wav"
 
 SWEP.Primary.ClipSize = -1      -- Size of a clip
 SWEP.Primary.DefaultClip = 0        -- Default number of bullets in a clip
@@ -71,13 +67,12 @@ function SWEP:PreDrawViewModel()
 end
 
 function SWEP:SecondaryAttack()
-  if not IsFirstTimePredicted() then return end
-  local Owner = self:GetOwner()
-
-  if not IsValid(Owner) then return end
-
-  self:SetNextPrimaryFire(CurTime() + 0.1)
-  if CLIENT then
+    if not IsFirstTimePredicted() then return end
+    local Owner = self:GetOwner()
+    if not IsValid(Owner) then return end
+    self:SetNextPrimaryFire(CurTime() + 0.1)
+    if SERVER then return end
+  
     local col1 = nil
     local col2 = nil
     local sel1 = nil
@@ -91,13 +86,13 @@ function SWEP:SecondaryAttack()
     saveButton:SetText("MIX!")
     saveButton:Dock(BOTTOM)
     function saveButton:DoClick()
-      if col1 and col2 then
-        net.Start("luctus_scp049_mixing")
-          net.WriteInt(col1,17)
-          net.WriteInt(col2,17)
-        net.SendToServer()
-        window:Close()
-      end
+        if col1 and col2 then
+            net.Start("luctus_scp049_mixing")
+                net.WriteInt(col1,17)
+                net.WriteInt(col2,17)
+            net.SendToServer()
+            window:Close()
+        end
     end
     local list1 = vgui.Create("DPanel",window)
     list1:Dock(LEFT)
@@ -113,97 +108,118 @@ function SWEP:SecondaryAttack()
     button = vgui.Create("DButton",list2)
     button:SetText("Second Mixture")
     button:Dock(TOP)
+    
     for k,v in pairs(scp049_colors) do
-      button = vgui.Create("DButton",list1)
-      button:Dock(TOP)
-      button:SetText(k)
-      button.selected = false
-      button.col = v[2]
-      button.id = v[1]
-      function button:Paint(w,h)
-        draw.RoundedBox(0,0,0,w,h,self.col)
-        if sel1 == self then
-          surface.SetDrawColor(0,0,0,255)
-          surface.DrawOutlinedRect(0, 0, w, h, 3)
+        button = vgui.Create("DButton",list1)
+        button:Dock(TOP)
+        button:SetText(k)
+        button.selected = false
+        button.col = v[2]
+        button.id = v[1]
+        function button:Paint(w,h)
+            draw.RoundedBox(0,0,0,w,h,self.col)
+            if sel1 == self then
+                surface.SetDrawColor(0,0,0,255)
+                surface.DrawOutlinedRect(0, 0, w, h, 3)
+            end
         end
-      end
-      function button:DoClick()
-        col1 = self.id
-        sel1 = self
-      end
+        function button:DoClick()
+            col1 = self.id
+            sel1 = self
+        end
     end
+    
     for k,v in pairs(scp049_colors) do
-      button = vgui.Create("DButton",list2)
-      button:Dock(TOP)
-      button:SetText(k)
-      button.selected = false
-      button.col = v[2]
-      button.id = v[1]
-      function button:Paint(w,h)
-        draw.RoundedBox(0,0,0,w,h,self.col)
-        if sel2 == self then
-          surface.SetDrawColor(0,0,0,255)
-          surface.DrawOutlinedRect(0, 0, w, h, 3)
+        button = vgui.Create("DButton",list2)
+        button:Dock(TOP)
+        button:SetText(k)
+        button.selected = false
+        button.col = v[2]
+        button.id = v[1]
+        function button:Paint(w,h)
+            draw.RoundedBox(0,0,0,w,h,self.col)
+            if sel2 == self then
+                surface.SetDrawColor(0,0,0,255)
+                surface.DrawOutlinedRect(0, 0, w, h, 3)
+            end
         end
-      end
-      function button:DoClick()
-        col2 = self.id
-        sel2 = self
-      end
+        function button:DoClick()
+            col2 = self.id
+            sel2 = self
+        end
     end
-  end
+end
+
+function SWEP:TouchKill(owner,target,tr)
+    if SCP049_SAVE_PMODELS[target:GetModel()] then return end
+    local edata = EffectData()
+    edata:SetStart(owner:GetShootPos())
+    edata:SetOrigin(tr.HitPos)
+    edata:SetNormal(tr.Normal)
+    edata:SetEntity(target)
+    util.Effect("BloodImpact", edata)
+    if CLIENT then return end
+    target:TakeDamage(99996, owner, self)
+    owner:EmitSound(self.KillSound)
+end
+
+function SWEP:OpenDoor(ply,ent,trace)
+    if CLIENT then return end
+    if hook.Call("canDoorRam", nil, ply, trace, ent) ~= nil then return end
+    
+    if SCP049_UNBREACHABLE[trace.Entity:GetName()] or SCP049_UNBREACHABLE[trace.Entity:MapCreationID()] then
+        DarkRP.notify(ply,1,5,"Please use '!breach' to initiate a breach!")
+        return false
+    end
+    
+    --SCP doors:
+    if ent:GetClass() == "prop_dynamic" and ent:GetParent() and IsValid(ent:GetParent()) and ent:GetParent():GetClass() == "func_door" then
+        ent = ent:GetParent()
+    end
+    ent:keysUnLock()
+    ent:Fire("open", "", 3)
+    ent:Fire("setanimation", "open", 3)
+    
+    hook.Run("onDoorRamUsed", true, ply, trace)
+    ply:EmitSound(self.Sound)
 end
 
 function SWEP:PrimaryAttack()
-  if not IsValid(self:GetOwner()) then return end
-  self:SetNextPrimaryFire(CurTime()+10)
-  self:GetOwner():LagCompensation(true)
-
-  local spos = self:GetOwner():GetShootPos()
-  local sdest = spos + (self:GetOwner():GetAimVector() * 70)
-
-  local kmins = Vector(1,1,1) * -10
-  local kmaxs = Vector(1,1,1) * 10
-
-  local tr = util.TraceHull({start=spos, endpos=sdest, filter=self:GetOwner(), mask=MASK_SHOT_HULL, mins=kmins, maxs=kmaxs})
-
-  -- Hull might hit environment stuff that line does not hit
-  if not IsValid(tr.Entity) then
-    tr = util.TraceLine({start=spos, endpos=sdest, filter=self:GetOwner(), mask=MASK_SHOT_HULL})
-  end
-
-  local hitEnt = tr.Entity
-
-  -- effects
-  if IsValid(hitEnt) then
-    self.Weapon:SendWeaponAnim( ACT_VM_HITCENTER )
-
-    local edata = EffectData()
-    edata:SetStart(spos)
-    edata:SetOrigin(tr.HitPos)
-    edata:SetNormal(tr.Normal)
-    edata:SetEntity(hitEnt)
-
-    if hitEnt:IsPlayer() or hitEnt:GetClass() == "prop_ragdoll" then
-       util.Effect("BloodImpact", edata)
+    if not IsValid(self:GetOwner()) then return end
+    self:SetNextPrimaryFire(CurTime()+0.2)
+    local ply = self:GetOwner()
+    local trace = ply:GetEyeTrace()
+    if ply:EyePos():Distance(trace.HitPos) > 512 then return end
+    local ent = trace.Entity
+    if not IsValid(ent) then return end
+    
+    --Kill Player
+    if ent:IsPlayer() and ent:Alive() then
+        self:TouchKill(ply,ent,trace)
+        return
     end
-  else
-    self.Weapon:SendWeaponAnim( ACT_VM_MISSCENTER )
-  end
-
-  if SERVER then
-    self:GetOwner():SetAnimation( PLAYER_ATTACK1 )
-  end
-
-
-  if SERVER and tr.Hit and tr.HitNonWorld and IsValid(hitEnt) then
-    if hitEnt:IsPlayer() or hitEnt:GetClass() == "prop_ragdoll" then
-      local owner = hitEnt:GetRagdollOwner()
-      local mixedFunc = scp049_effect_functions[scp049_mixtable[scp049_col_one][scp049_col_two]]
-      self.Owner:PrintMessage(HUD_PRINTTALK, "Your mixture was '"..scp049_mixtable[scp049_col_one][scp049_col_two].."'")
-      mixedFunc(owner)
+    --Open Door
+    if ent:isDoor() then
+        self:OpenDoor(ply,ent,trace)
+        return
     end
-  end
-
-  self:GetOwner():LagCompensation(false)
+    --Zombify
+    if ent:GetClass() == "prop_ragdoll" and SERVER then
+        if not ply.scp049_col_one or not ply.scp049_col_two then return end
+        local deadPlayer = ent:GetRagdollOwner()
+        --if gDeathsystem:
+        if MedConfig then
+            local ragdoll = ent
+            for k,v in pairs(player.GetAll()) do
+                if v:GetNW2Entity("RagdollEntity",nil) == ragdoll then
+                    deadPlayer = v
+                end
+            end
+        end
+        
+        print("[DEBUG]",ply.scp049_col_one,ply.scp049_col_two,scp049_mixtable[ply.scp049_col_one][ply.scp049_col_two])
+        local mixedFunc = scp049_effect_functions[scp049_mixtable[ply.scp049_col_one][ply.scp049_col_two]]
+        ply:PrintMessage(HUD_PRINTTALK, "Your mixture was '"..scp049_mixtable[ply.scp049_col_one][ply.scp049_col_two].."'")
+        LuctusSCP049SpawnZombie(mixedFunc,deadPlayer)
+    end
 end
