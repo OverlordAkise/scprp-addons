037c2df315bfb13ce515ae794a05d76a9347868c
OverlordAkise <OverlordAkise@gmail.com>
2023-08-15 14:59:12 +0200

    [breach] Removed not-needed cl file

--- luctus_breach/lua/autorun/client/cl_breach.lua
+++
@@ -1,8 +0,0 @@
---Luctus Breach
---Made by OverlordAkise
-
-hook.Add("LuctusLogAddCategory","luctus_breach",function()
-    table.insert(lucid_log_quickfilters,"Breach")
-end)
-
-print("[luctus_breach] cl loaded!")
