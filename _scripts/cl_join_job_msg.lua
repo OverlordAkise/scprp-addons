c572974316da03fd6f1a3b64d803ad5802992b06
OverlordAkise <OverlordAkise@gmail.com>
2023-08-26 13:32:06 +0200

    [scripts] Optimized join_job_msg

--- _scripts/cl_join_job_msg.lua
+++ _scripts/cl_join_job_msg.lua
@@ -4,8 +4,16 @@
 --Because 3 people asked me for it already
 
 hook.Add("InitPostEntity","luctus_joinjob_msg",function()
+    local shouldDisplay = false
+    timer.Create("luctus_joinjob_msg",1,0,function()
+        if LocalPlayer():Team() != GAMEMODE.DefaultTeam then
+            shouldDisplay = false
+        else
+            shouldDisplay = true
+        end
+    end)
     hook.Add("HUDPaint","luctus_joinjob_msg",function()
-        if LocalPlayer():Team() != GAMEMODE.DefaultTeam then return end
+        if not shouldDisplay then return end
         draw.SimpleTextOutlined("Press F4 to change your job and join the RP.","Trebuchet24", ScrW()/2,ScrW()/15,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP,2,color_black)
     end)
 end)
