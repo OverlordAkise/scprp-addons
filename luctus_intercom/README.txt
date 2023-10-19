339f7e7b2914e7c85bfcc41559f2e7459354eade
OverlordAkise <OverlordAkise@gmail.com>
2023-10-19 15:25:49 +0200

    [intercom] Quick-Fixed the global hearing bug

---
+++ luctus_intercom/README.txt
@@ -0,0 +1,11 @@
+# luctus_intercom
+
+To make it work you have to set up the "box" that includes the whole foundation.
+
+To set up this box fly (with noclip) to the lowest corner on one end of the map and say "!intercom1" in chat.  
+Then fly to the other corner above the foundation (but not including the surface) and say "!intercom2" in chat.  
+To now load this box as your foundation simply type "!intercomreload" in chat.
+
+For a more visual representation of how this box should look like take a look at this wiki entry for the function that checks it:
+
+https://wiki.facepunch.com/gmod/Vector:WithinAABox
--- luctus_intercom/lua/autorun/server/sv_intercom.lua
+++ luctus_intercom/lua/autorun/server/sv_intercom.lua
@@ -1,14 +1,57 @@
 --Luctus Intercom
 --Made by OverlordAkise
 
-LUCTUS_INTERCOM_LISTENERS = {}
+LUCTUS_INTERCOM_LISTENERS = LUCTUS_INTERCOM_LISTENERS or {}
+LUCTUS_INTERCOM_SPEAKERS = LUCTUS_INTERCOM_SPEAKERS or {}
+LUCTUS_INTERCOM_POS_ONE = LUCTUS_INTERCOM_POS_ONE or nil
+LUCTUS_INTERCOM_POS_TWO = LUCTUS_INTERCOM_POS_TWO or nil
 
 util.AddNetworkString("luctus_intercom_sync")
 
+hook.Add("PlayerSay","luctus_intercom",function(ply,text)
+    if not ply:IsAdmin() then return end
+    if text == "!intercom1" then
+        LuctusIntercomSaveLocation(ply:GetPos(),true)
+        DarkRP.notify(ply,0,5,"First position saved!")
+    end
+    if text == "!intercom2" then
+        LuctusIntercomSaveLocation(ply:GetPos(),false)
+        DarkRP.notify(ply,0,5,"Second position saved!")
+    end
+    if text == "!intercomreload" then
+        LuctusIntercomReloadPos()
+        DarkRP.notify(ply,0,5,"Reloaded positions!")
+    end
+end)
+
+function LuctusIntercomSaveLocation(vector,isFirst)
+    if isFirst then
+        file.Write("luctus_intercom_vec1.txt",tostring(vector))
+    else
+        file.Write("luctus_intercom_vec2.txt",tostring(vector))
+    end
+end
+
+function LuctusIntercomReloadPos()
+    if file.Exists("luctus_intercom_vec1.txt","DATA") and file.Exists("luctus_intercom_vec2.txt","DATA") then
+        LUCTUS_INTERCOM_POS_ONE = Vector(file.Read("luctus_intercom_vec1.txt","DATA"))
+        LUCTUS_INTERCOM_POS_TWO = Vector(file.Read("luctus_intercom_vec2.txt","DATA"))
+    else
+        ErrorNoHaltWithStack("INTERCOM ERROR: NO POSITIONS FOUND!")
+        print("[intercom] Please use '!intercom1' and '!intercom2' to set the positions")
+        print("[intercom] of the box where players can hear the intercom!")
+        print("[intercom] Then use '!intercomreload' to reload the positions")
+    end
+end
+
+hook.Add("InitPostEntity","luctus_intercom",function()
+    LuctusIntercomReloadPos()
+end)
+
 function LuctusIntercomAdd(ply,ent)
-    if not LUCTUS_INTERCOM_LISTENERS[ply] then
+    if not LUCTUS_INTERCOM_SPEAKERS[ply] then
         ent.Speakers[ply] = true
-        LUCTUS_INTERCOM_LISTENERS[ply] = true
+        LUCTUS_INTERCOM_SPEAKERS[ply] = true
         net.Start("luctus_intercom_sync")
             net.WriteBool(true)
         net.Send(ply)
@@ -18,20 +61,33 @@ end
 function LuctusIntercomRemoveCheck(ply,ent,forceDelete)
     if not IsValid(ply) then
         ent.Speakers[ply] = nil
-        LUCTUS_INTERCOM_LISTENERS[ply] = nil
+        LUCTUS_INTERCOM_SPEAKERS[ply] = nil
         return
     end
     if ply:GetPos():Distance(ent:GetPos()) > 256 or forceDelete then
         ent.Speakers[ply] = nil
-        LUCTUS_INTERCOM_LISTENERS[ply] = nil
+        LUCTUS_INTERCOM_SPEAKERS[ply] = nil
         net.Start("luctus_intercom_sync")
             net.WriteBool(false)
         net.Send(ply)
     end
 end
 
+timer.Create("luctus_intercom",1,0,function()
+    LUCTUS_INTERCOM_LISTENERS = {}
+    if table.Count(LUCTUS_INTERCOM_SPEAKERS)<1 then
+        return
+    end
+    if not LUCTUS_INTERCOM_POS_ONE or not LUCTUS_INTERCOM_POS_TWO then return end
+    for k,ply in ipairs(player.GetAll()) do
+        if ply:GetPos():WithinAABox(LUCTUS_INTERCOM_POS_ONE,LUCTUS_INTERCOM_POS_TWO) then
+            LUCTUS_INTERCOM_LISTENERS[ply] = true
+        end
+    end
+end)
+
 hook.Add("PlayerCanHearPlayersVoice","luctus_intercom_global_talk",function(listener, talker)
-    if LUCTUS_INTERCOM_LISTENERS[ply] then
+    if LUCTUS_INTERCOM_SPEAKERS[talker] and LUCTUS_INTERCOM_LISTENERS[listener] then
         return true, false
     end
 end)
