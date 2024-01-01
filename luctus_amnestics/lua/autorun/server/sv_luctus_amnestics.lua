38315607acdfcda7bdd0071a9f4b9111573084ae
OverlordAkise <OverlordAkise@gmail.com>
2024-01-01 14:36:25 +0100

    [amnestics] Added hook for using amnestics

--- luctus_amnestics/lua/autorun/server/sv_luctus_amnestics.lua
+++ luctus_amnestics/lua/autorun/server/sv_luctus_amnestics.lua
@@ -4,8 +4,6 @@
 resource.AddWorkshop("2595071184")
 util.AddNetworkString("luctus_amnestics")
 
-LuctusLog = LuctusLog or function()end
-
 function LuctusSendAmnesticsEffect(sending_ply, target_ply, amnestic_type)
     --testing: target_ply = sending_ply
     if not IsValid(target_ply) or not IsValid(sending_ply) then return end
@@ -16,7 +14,7 @@ function LuctusSendAmnesticsEffect(sending_ply, target_ply, amnestic_type)
         net.WriteString(amnestic_type)
     net.Send(target_ply)
     
-    LuctusLog("SCP",string.format("%s (%s) has given amnestic type %s to %s (%s)",sending_ply:Nick(),sending_ply:SteamID(),amnestic_type,target_ply:Nick(),target_ply:SteamID()))
+    hook.Run("LuctusAmnestics",sending_ply,target_ply,amnestic_type)
 end
 
 print("[luctus_amnestics] sv loaded!")
