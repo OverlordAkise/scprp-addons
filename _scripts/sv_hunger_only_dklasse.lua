b07e71bacb19a6eb54eb91e396bc214a76722e63
OverlordAkise <OverlordAkise@gmail.com>
2024-01-15 19:16:17 +0100

    [_scripts] Update hunger script with jobconfig

--- _scripts/sv_hunger_only_dklasse.lua
+++ _scripts/sv_hunger_only_dklasse.lua
@@ -1,32 +1,43 @@
 --Luctus Hunger-Only-For-DKlasse-Fix
 --Made by OverlordAkise
 
+--Jobs who count as a cook
+local cookjobs = {
+    ["Koch"] = true,
+    ["Cook"] = true,
+}
+--Jobs who count as D-Class
+local dclassjobs = {
+    ["D-Klasse"] = true,
+    ["Cook"] = true,
+}
+
 --Hungerspeed config is at addons/darkrpmodification/lua/darkrp_config/settings.lua at the bottom
 --This function makes every job not called "D-Klasse" not being hungry
---Also, you only have hunger if a job called "Koch" is on the server and a player is playing it
+--Also, you only have hunger if a job called "Cook" is on the server and a player is playing it
 
 hook.Add("PlayerInitialSpawn", "luctus_fix_hunger_dklasse_only", function(ply)
-    if timer.Exists("HMThink") then
-        timer.Remove("HMThink")
-        --https://wiki.facepunch.com/gmod/timer.Remove
-        timer.Simple(0.1, function()
-            timer.Create("HMThink", 10, 0, function()
-                local a = nil
-                for k,v in pairs(player.GetAll()) do
-                    if (v:getJobTable().name == "Koch") then
-                        a = true
-                        break
-                    end
+    if not timer.Exists("HMThink") then return end
+    timer.Remove("HMThink")
+    --https://wiki.facepunch.com/gmod/timer.Remove
+    timer.Simple(0.1, function()
+        timer.Create("HMThink", 10, 0, function()
+            local cookOnline = nil
+            for k,ply in ipairs(player.GetAll()) do
+                if cookjobs[team.GetName(ply:Team())] then
+                    cookOnline = true
+                    break
                 end
-                if !a then return end 
+            end
+            if not cookOnline then return end 
 
-                for _, v in ipairs(player.GetAll()) do 
-                    if not v:Alive() then continue end 
-                    if not string.find(v:getJobTable().name,"D-Klasse") then continue end 
-                    v:hungerUpdate() 
-                end 
-            end)
+            for _,ply in ipairs(player.GetHumans()) do 
+                if not ply:Alive() then continue end 
+                if not dclassjobs[team.GetName(ply:Team())] then continue end 
+                ply:hungerUpdate() 
+            end 
         end)
-    end
+    end)
     hook.Remove("PlayerInitialSpawn", "luctus_fix_hunger_dklasse_only")
+    print("[luctus_hunger] Hunger fix loaded")
 end)
