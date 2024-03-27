90397120187a9ffc5d79d98e099c36549208366c
OverlordAkise <OverlordAkise@gmail.com>
2024-03-27 16:23:13 +0100

    [jobnumbers] Added config to hide names

--- luctus_jobnumbers/lua/autorun/sh_jobnums_config.lua
+++ luctus_jobnumbers/lua/autorun/sh_jobnums_config.lua
@@ -8,12 +8,13 @@ LUCTUS_JOBNUMBERS_USERS_CAN_CHANGE = true
 
 --[JobName] = {Prefix,MinValue,MaxValue}
 --%d gets replaced by the number
+--You can use %06d to pad the number to 6 digits (with 0 infront)
 LUCTUS_JOBNUMBERS["D-Klasse"] = {"[D-%d]",1000,9999}
 LUCTUS_JOBNUMBERS["O5"] = {"[O5-%d]",1,15}
 LUCTUS_JOBNUMBERS["Citizen"] = {"[C-%d]",100,999}
 LUCTUS_JOBNUMBERS["Medic"] = {"[M-%d]",100,999}
 LUCTUS_JOBNUMBERS["Gun Dealer"] = {"[GD-%d]",100,999}
-LUCTUS_JOBNUMBERS["Hobo"] = {"[USELESS]",100,999}
+LUCTUS_JOBNUMBERS["Hobo"] = {"[USELESS-%04d]",100,999}
 
 --Which jobs share the same numbers
 --left side is an id and right are the jobnames who share the number
@@ -22,6 +23,21 @@ LUCTUS_JOBNUMBERS["Hobo"] = {"[USELESS]",100,999}
 LUCTUS_JOBNUMBERS_SHAREID = {
     ["civilianids"] = {"Medic","Gun Dealer"},
 }
+
+--Hide the name of a player with the specific job
+LUCTUS_JOBNUMBERS_HIDE_NICK = {
+    ["O5"] = true,
+    ["Hobo"] = true,
+    ["Medic"] = true,
+}
+--The following jobs can see the nick of a hidden player
+LUCTUS_JOBNUMBERS_SHOW_HIDDEN_NICK = {
+    ["O5"] = true,
+    ["Site Director"] = true,
+    ["Medic"] = true,
+}
+--Which name should be shown when hidden?
+LUCTUS_JOBNUMBERS_HIDDEN_NICK = "<REDACTED>"
 
 
 print("[luctus_jobnumbers] config loaded!")
--- luctus_jobnumbers/lua/autorun/sh_jobnums_nick.lua
+++ luctus_jobnumbers/lua/autorun/sh_jobnums_nick.lua
@@ -7,6 +7,8 @@ hook.Add("loadCustomDarkRPItems", "z_luctus_jn_nick_overwrite", function()
     PLAYER.OldName = PLAYER.Name
     function PLAYER:Name()
         if not self:IsValid() then DarkRP.error("Attempt to call Name/Nick/GetName on a non-existing player!", SERVER and 1 or 2) end
+        local jobName = team.GetName(self:Team())
+        if LUCTUS_JOBNUMBERS_HIDE_NICK[jobName] and not LUCTUS_JOBNUMBERS_SHOW_HIDDEN_NICK[jobName] then return LUCTUS_JOBNUMBERS_HIDDEN_NICK end
         local Nick = self:OldName()
         if self:GetNWString("l_numtag","") ~= "" then
             Nick = self:GetNWString("l_numtag","") .. " " .. Nick
