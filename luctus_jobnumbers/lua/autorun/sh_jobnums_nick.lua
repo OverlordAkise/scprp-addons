665e5c7c826fe864c5c2a56e4b8021b1b37890ff
OverlordAkise <OverlordAkise@gmail.com>
2024-03-29 14:59:24 +0100

    [jobnumbers] Fixed hidden names not working

--- luctus_jobnumbers/lua/autorun/sh_jobnums_nick.lua
+++ luctus_jobnumbers/lua/autorun/sh_jobnums_nick.lua
@@ -7,8 +7,7 @@ hook.Add("loadCustomDarkRPItems", "z_luctus_jn_nick_overwrite", function()
     PLAYER.OldName = PLAYER.Name
     function PLAYER:Name()
         if not self:IsValid() then DarkRP.error("Attempt to call Name/Nick/GetName on a non-existing player!", SERVER and 1 or 2) end
-        local jobName = team.GetName(self:Team())
-        if LUCTUS_JOBNUMBERS_HIDE_NICK[jobName] and not LUCTUS_JOBNUMBERS_SHOW_HIDDEN_NICK[jobName] then return LUCTUS_JOBNUMBERS_HIDDEN_NICK end
+        if CLIENT and LUCTUS_JOBNUMBERS_HIDE_NICK[team.GetName(self:Team())] and not LUCTUS_JOBNUMBERS_SHOW_HIDDEN_NICK[team.GetName(LocalPlayer():Team())] then return LUCTUS_JOBNUMBERS_HIDDEN_NICK end
         local Nick = self:OldName()
         if self:GetNWString("l_numtag","") ~= "" then
             Nick = self:GetNWString("l_numtag","") .. " " .. Nick
