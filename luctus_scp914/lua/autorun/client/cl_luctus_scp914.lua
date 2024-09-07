c766331e515da76f9754e3ca0ff62ac13d73f708
OverlordAkise <OverlordAkise@gmail.com>
2024-09-08 00:40:14 +0200

    [scp914] Rework, support for Site65

--- luctus_scp914/lua/autorun/client/cl_luctus_scp914.lua
+++ luctus_scp914/lua/autorun/client/cl_luctus_scp914.lua
@@ -1,159 +1,158 @@
 --Luctus SCP914 System
 --Made by OverlordAkise
 
-if (game.GetMap() ~= "rp_liquidrp_ct_site99") then 
-  print("[luctus_scp914] THIS ADDON ONLY WORKS ON THE MAP rp_liquidrp_ct_site99 ! Canceling loading!")
-  return 
+net.Receive("luctus_scp914_nomap",function()
+    Derma_Message("This current map is not supported! SCP914 will NOT work with this addon!", "luctus scp914 | unsupported map", "OK")
+end)
+
+local frame
+local scp914Liste = nil
+net.Receive("luctus_scp914_get",function()
+    local lenge = net.ReadInt(17)
+    local data = net.ReadData(lenge)
+    local jtext = util.Decompress(data)
+    local tab = util.JSONToTable(jtext)
+    if tab == nil then return end
+
+    if not IsValid(scp914Liste) then return end
+    scp914Liste:Clear()
+    for k,row in ipairs(tab) do
+        scp914Liste:AddLine(row.rowid, row.input, row.output, row.wheelnum)
+    end
+end)
+
+local function createLabel(text,parent)
+    local arrowLabel = parent:Add("DLabel")
+    arrowLabel:SetText(text)
+    arrowLabel:Dock(LEFT)
+    arrowLabel:SetFont("Trebuchet18")
+    arrowLabel:SetTextColor(Color(255, 255, 255))
+    arrowLabel:SetWide(30)
+    if string.len(text) > 4 then
+        arrowLabel:SetWide(70)
+    end
 end
 
-
-local scp914Liste = nil
-local scp914CurType = 1
-net.Receive("luctus_scp914_get",function()
-  local lenge = net.ReadInt(17)
-  local data = net.ReadData(lenge)
-  local jtext = util.Decompress(data)
-  local tab = util.JSONToTable(jtext)
-  if tab == nil then return end
-  
-  if IsValid(scp914Liste) then
-    scp914Liste:Clear()
-    for k,v in pairs(tab) do
-      scp914Liste:AddLine( v.rowid, v.input, v.output )
-    end
-  end
-end)
 concommand.Add("scp914_config", function(ply, cmd, args)
-  if not ply:IsValid() or not ply:IsAdmin() then return end
-  if IsValid(frame) then
-    frame:Remove()
-  end
+    if not ply:IsValid() or not ply:IsAdmin() then return end
+    if IsValid(frame) then
+        frame:Remove()
+    end
   
-  frame = vgui.Create("DFrame")
-  frame:SetTitle("SCP-914 Settings")
-  frame:SetSize(450, 600)
-  frame:Center()
-  frame:MakePopup()
-  frame.btnMaxim:SetVisible(false)
-  frame.btnMinim:SetVisible(false)
-  
-  
-  --PANEL TOP
-  local top = frame:Add("Panel")
-  top:Dock(TOP)
-  
-  local credit = top:Add("DLabel")
-  credit:SetText("SCP914 Config für Liquid's Site99")
-  credit:Dock(LEFT)
-  credit:SetTextColor(Color(255, 255, 255))
-  credit:SetFont("DermaDefaultBold")
-  credit:SetWide(300)
-  
-  local combo = top:Add("DComboBox")
-  combo:SetValue("Select a type")
-  combo:AddChoice("Rough", 1)
-  combo:AddChoice("Coarse", 2)
-  combo:AddChoice("1:1", 3)
-  combo:AddChoice("Fine", 4)
-  combo:AddChoice("Very Fine", 5)
-  combo:Dock(RIGHT)
-  combo:SetWide(150)
-  combo.OnSelect = function(self, index, value, data)
-    --data = id
-    scp914CurType = tonumber(data)
-    net.Start("luctus_scp914_get")
-      net.WriteInt(data,8)
-    net.SendToServer()
-  end
+    frame = vgui.Create("DFrame")
+    frame:SetTitle("Luctus' SCP914 Config")
+    frame:SetSize(450, 600)
+    frame:Center()
+    frame:MakePopup()
+    frame.btnMaxim:SetVisible(false)
+    frame.btnMinim:SetVisible(false)
   
   
-  --PANEL BOT
-  local bot = frame:Add("Panel")
-  bot:Dock(BOTTOM)
+    --PANEL TOP
+    local top = frame:Add("Panel")
+    top:Dock(TOP)
+
+    local hint = top:Add("DLabel")
+    hint:SetText("HINT: Wheel position starts at 0, if there are 5 positions => 0,1,2,3,4")
+    hint:Dock(LEFT)
+    hint:SetTextColor(Color(255, 255, 255))
+    hint:SetFont("Trebuchet18")
+    hint:SetWide(450)
   
-  local inputEntry = bot:Add("DTextEntry")
-  inputEntry:SetPlaceholderText("Input class")
-  inputEntry:Dock(LEFT)
   
-  local arrowLabel = bot:Add("DLabel")
-  arrowLabel:SetText("→")
-  arrowLabel:Dock(LEFT)
-  arrowLabel:SetContentAlignment(5)
-  arrowLabel:SetFont("Trebuchet18")
-  arrowLabel:SetTextColor(Color(255, 255, 255))
-  
-  local outputEntry = bot:Add("DTextEntry")
-  outputEntry:SetPlaceholderText("Output class")
-  outputEntry:Dock(LEFT)
-  
-  local insertBtn = bot:Add("DButton")
-  insertBtn:Dock(RIGHT)
-  insertBtn:SetText("Add")
-  insertBtn.DoClick = function()
-    local input = string.Trim(inputEntry:GetValue())
-    local output = string.Trim(outputEntry:GetValue())
+    --PANEL BOT
+    local bot = frame:Add("Panel")
+    bot:Dock(BOTTOM)
     
-    if #input == 0 or #output == 0 then return end
-    local wType = combo:GetOptionData(combo:GetSelectedID())
-    if not wType then return end
-    net.Start("luctus_scp914_save")
-      net.WriteString(input)
-      net.WriteString(output)
-      net.WriteInt(combo:GetOptionData(combo:GetSelectedID()), 8)
-    net.SendToServer()
-    timer.Simple(0.1,function()
-      net.Start("luctus_scp914_get")
-        net.WriteInt(scp914CurType,8)
-      net.SendToServer()
-    end)
-  end
+    createLabel("wheel pos:",bot)
+    local wheelEntry = bot:Add("DTextEntry")
+    wheelEntry:SetPlaceholderText("1")
+    wheelEntry:Dock(LEFT)
+    wheelEntry:SetDrawLanguageID(false)
+    
+    createLabel("//",bot)
+    local inputEntry = bot:Add("DTextEntry")
+    inputEntry:SetPlaceholderText("Input")
+    inputEntry:Dock(LEFT)
+    inputEntry:SetDrawLanguageID(false)
+
+    createLabel("→",bot)
+    local outputEntry = bot:Add("DTextEntry")
+    outputEntry:SetPlaceholderText("Output")
+    outputEntry:Dock(LEFT)
+    outputEntry:SetDrawLanguageID(false)
+
   
-  bot.PerformLayout = function(self, w, h)
-    inputEntry:SetWide(154)
-    outputEntry:SetWide(154)
-    arrowLabel:SetWide(44)
-    insertBtn:SetWide(66)
-  end
-  
-  --PANEL LISTE
-  scp914Liste = frame:Add("DListView")
-  scp914Liste:Dock(FILL)
-  scp914Liste:AddColumn("ID"):SetWidth(30)
-  scp914Liste:AddColumn("Input"):SetWidth(210)
-  scp914Liste:AddColumn("Output")	:SetWidth(210)
-  scp914Liste.OnRowRightClick = function(self, lineId, line)
-    local menu = DermaMenu() 
-    menu:AddOption("Remove", function()
-      net.Start("luctus_scp914_delete")
-        net.WriteInt(tonumber(line:GetColumnText(1)), 32)
-      net.SendToServer()
-      timer.Simple(0.1,function()
-        net.Start("luctus_scp914_get")
-          net.WriteInt(scp914CurType,8)
+    local insertBtn = bot:Add("DButton")
+    insertBtn:Dock(RIGHT)
+    insertBtn:SetText("Add")
+    insertBtn.DoClick = function()
+        local input = string.Trim(inputEntry:GetValue())
+        local output = string.Trim(outputEntry:GetValue())
+        local wheelStr = wheelEntry:GetValue()
+        local wheelnum = tonumber(wheelStr)
+        if not wheelnum then return end
+        
+        if #input == 0 or #output == 0 then return end
+        net.Start("luctus_scp914_save")
+            net.WriteString(input)
+            net.WriteString(output)
+            net.WriteInt(wheelnum, 8)
         net.SendToServer()
-      end)
-    end):SetIcon( "icon16/delete.png" )
-    menu:AddOption("Edit", function()
-      local inputEnt = line:GetColumnText(2) or ""
-      local outputEnt = line:GetColumnText(2) or ""
-      Derma_StringRequest("Input Entity", "What entity should be put in...", inputEnt, function(text) 
-        inputEnt = text 
-        Derma_StringRequest("Output Entity", "... and what entity should be the output?", outputEnt, function(text) 
-          outputEnt = text
-          net.Start("luctus_scp914_edit")
-            net.WriteString(inputEnt)
-            net.WriteString(outputEnt)
-            net.WriteInt(combo:GetOptionData(combo:GetSelectedID()), 8)
-            net.WriteInt(tonumber(line:GetColumnText(1)), 32)
-          net.SendToServer()
-          timer.Simple(0.1,function()
+        timer.Simple(0.1,function()
             net.Start("luctus_scp914_get")
-              net.WriteInt(scp914CurType,8)
             net.SendToServer()
-          end)
-        end, function(text) print("") end)
-      end, function(text) print("") end)
-    end):SetIcon( "icon16/arrow_rotate_clockwise.png" )
-    menu:Open()
-  end
+        end)
+    end
+  
+    --PANEL LISTE
+    scp914Liste = frame:Add("DListView")
+    scp914Liste:Dock(FILL)
+    scp914Liste:AddColumn("ID"):SetWidth(30)
+    scp914Liste:AddColumn("Input"):SetWidth(210)
+    scp914Liste:AddColumn("Output"):SetWidth(210)
+    scp914Liste:AddColumn("Wheel"):SetWidth(50)
+    function scp914Liste:OnRowRightClick(lineId, line)
+        local menu = DermaMenu() 
+        menu:AddOption("Remove", function()
+            net.Start("luctus_scp914_delete")
+                net.WriteInt(tonumber(line:GetColumnText(1)), 32)
+            net.SendToServer()
+            timer.Simple(0.1,function()
+                net.Start("luctus_scp914_get")
+                net.SendToServer()
+            end)
+        end):SetIcon( "icon16/delete.png" )
+        menu:AddOption("Edit", function()
+            local inputEnt = line:GetColumnText(2) or ""
+            local outputEnt = line:GetColumnText(3) or ""
+            local wheelStr = line:GetColumnText(4) or ""
+            Derma_StringRequest("Input Entity", "input...", inputEnt, function(text) 
+                inputEnt = text 
+                Derma_StringRequest("Output Entity", "...output...", outputEnt, function(text) 
+                    outputEnt = text
+                    Derma_StringRequest("Wheel position","...wheel position", wheelStr, function(text)
+                        wheelStr = text
+                        local wheelNum = tonumber(wheelStr)
+                        if not wheelNum then return end
+                        net.Start("luctus_scp914_edit")
+                            net.WriteString(inputEnt)
+                            net.WriteString(outputEnt)
+                            net.WriteInt(wheelNum, 8)
+                            net.WriteInt(tonumber(line:GetColumnText(1)), 32)
+                        net.SendToServer()
+                        timer.Simple(0.1,function()
+                            net.Start("luctus_scp914_get")
+                            net.SendToServer()
+                        end)
+                    end, function()end)
+                end, function()end)
+            end, function()end)
+        end):SetIcon("icon16/arrow_rotate_clockwise.png")
+        menu:Open()
+    end
+    net.Start("luctus_scp914_get")
+    net.SendToServer()
 end)
+
+print("[luctus_scp914] cl loaded")
--- luctus_scp914/lua/autorun/server/sv_luctus_scp914.lua
+++ luctus_scp914/lua/autorun/server/sv_luctus_scp914.lua
@@ -5,148 +5,183 @@
 --EDIT BUTTON IN MENU
 
 
-if (game.GetMap() ~= "rp_liquidrp_ct_site99") then 
-  error("[luctus_scp914] THIS ADDON IS ONLY MADE FOR rp_liquidrp_ct_site99 ! Canceling loading!")
-end
+local supported_maps = {
+    ["rp_liquidrp_ct_site99"] = true,
+    ["rp_site65"] = true,
+}
 
 util.AddNetworkString("luctus_scp914_save")
 util.AddNetworkString("luctus_scp914_edit")
 util.AddNetworkString("luctus_scp914_delete")
 util.AddNetworkString("luctus_scp914_get")
+util.AddNetworkString("luctus_scp914_nomap")
 
 hook.Add("InitPostEntity","luctus_scp914_setup",function()
-  local res = sql.Query("CREATE TABLE IF NOT EXISTS luctus_scp914('input' TEXT NOT NULL, 'output' TEXT NOT NULL, 'wheeltype' INTEGER NOT NULL);")
-  if res ~= false then
-    print("[luctus_scp914] Successfully setup DB!")
-  end
+    local res = sql.Query("CREATE TABLE IF NOT EXISTS luctus_scp914(input TEXT, output TEXT, wheelnum INTEGER, map TEXT);")
+    if res == false then
+        error(sql.LastError())
+    end
 end)
 
-net.Receive("luctus_scp914_delete", function(len, ply)
-  if not ply:IsValid() or not ply:IsAdmin() then return end
-  local rowID = net.ReadInt(32)
-  local res = sql.Query("DELETE FROM luctus_scp914 WHERE rowid = "..rowID)
-  if res ~= false then
-    print("[luctus_scp914] Successfully deleted from list!")
-    print("[luctus_scp914] Deleted entry ID: "..rowID)
-    ply:PrintMessage(HUD_PRINTTALK, "[scp914] Deleted successfully!")
-  else
-    ply:PrintMessage(HUD_PRINTTALK, "[scp914] Error during delete, please contact an admin!")
-    print("[luctus_scp914] Error during delete from list! :")
-    print(sql.LastError())
-  end
-end)
 
-function getSCP914s(wheeltype)
-  if not tonumber(wheeltype) then return {} end
-  local ret = sql.Query("SELECT rowid,* FROM luctus_scp914 WHERE wheeltype = "..wheeltype)
-  if(ret==false)then
-    print("[luctus_scp914] SQL ERROR DURING getSCP914s!")
-    print(sql.LastError())
+function LuctusSCP914GetConfigForMap(curMap)
+    local ret = sql.Query("SELECT rowid,* FROM luctus_scp914 WHERE map="..sql.SQLStr(curMap))
+    if ret==false then
+        ErrorNoHaltWithStack(sql.LastError())
+        return {}
+    end
+    if ret then
+        return ret
+    end
     return {}
-  end
-  if ret and ret ~= nil then
-    --print("Response!")
-    --print("Sending:")
-    --PrintTable(ret)
-    return ret
-  end
-  return {}
 end
 
+function LuctusSCP914GetOutput(input,wheel,map)
+    local res = sql.QueryValue("SELECT output FROM luctus_scp914 WHERE input="..sql.SQLStr(input).." AND wheelnum="..wheel.." AND map="..sql.SQLStr(map))
+    if res == false then
+        ErrorNoHaltWithStack(sql.LastError())
+        return nil
+    end
+    if type(res) ~= "string" then return nil end
+    return res
+end
+
+net.Receive("luctus_scp914_delete", function(len, ply)
+    if not ply:IsValid() or not ply:IsAdmin() then return end
+    local rowID = net.ReadInt(32)
+    local res = sql.Query("DELETE FROM luctus_scp914 WHERE rowid = "..rowID)
+    if res ~= false then
+        print("[luctus_scp914] Successfully deleted from list! "..rowID)
+        ply:PrintMessage(HUD_PRINTTALK, "[scp914] Deleted successfully!")
+    else
+        ErrorNoHaltWithStack(sql.LastError())
+        ply:PrintMessage(HUD_PRINTTALK, "[scp914] Error during delete!")
+    end
+    hook.Run("LuctusSCP914ConfigDeleted",ply,rowID)
+end)
+
 net.Receive("luctus_scp914_get", function(len, ply)
-  if not ply:IsValid() or not ply:IsAdmin() then return end
-  local wheeltype = net.ReadInt(8)
-  local liste = getSCP914s(wheeltype)
-  net.Start("luctus_scp914_get")
-    local t = util.TableToJSON(liste)
-    local a = util.Compress(t)
-    net.WriteInt(#a,17)
-    net.WriteData(a,#a)
-  net.Send(ply)
+    if not ply:IsValid() or not ply:IsAdmin() then return end
+    local curMap = game.GetMap()
+    if not supported_maps[curMap] then
+        net.Start("luctus_scp914_nomap") net.Send(ply)
+        return
+    end
+    local liste = LuctusSCP914GetConfigForMap(curMap)
+    net.Start("luctus_scp914_get")
+        local a = util.Compress(util.TableToJSON(liste))
+        net.WriteInt(#a,17)
+        net.WriteData(a,#a)
+    net.Send(ply)
 end)
 
 net.Receive("luctus_scp914_edit", function(len, ply)
-  if not ply:IsValid() or not ply:IsAdmin() then return end
-  local inputEnt = net.ReadString()
-  local outputEnt = net.ReadString()
-  local wheeltype = net.ReadInt(8)
-  local rowID = net.ReadInt(32)
-  local res = sql.Query("UPDATE luctus_scp914 SET input = "..sql.SQLStr(inputEnt)..", output = "..sql.SQLStr(outputEnt).." WHERE rowid = "..rowID)
+    if not ply:IsValid() or not ply:IsAdmin() then return end
+    local inputEnt = net.ReadString()
+    local outputEnt = net.ReadString()
+    local wheelnum = net.ReadInt(8)
+    local rowID = net.ReadInt(32)
+    local res = sql.Query("UPDATE luctus_scp914 SET input = "..sql.SQLStr(inputEnt)..", output = "..sql.SQLStr(outputEnt)..", wheelnum = "..sql.SQLStr(wheelnum).." WHERE rowid = "..rowID)
 
-  if res ~= false then
-    print("[luctus_scp914] Successfully updated list!")
-    print("[luctus_scp914] Changed entry to: "..inputEnt.." / "..outputEnt.." / "..wheeltype)
-    ply:PrintMessage(HUD_PRINTTALK, "[scp914] Changed successfully!")
-  else
-    ply:PrintMessage(HUD_PRINTTALK, "[scp914] Error during change, please contact an admin!")
-    print("[luctus_scp914] Error during edit list! :")
-    print(sql.LastError())
-  end
+    if res ~= false then
+        print("[luctus_scp914] Successfully updated list!")
+        print("[luctus_scp914] Changed entry to: ",inputEnt," -> ",outputEnt," / ",wheelnum)
+        ply:PrintMessage(HUD_PRINTTALK, "[scp914] Changed successfully!")
+    else
+        ply:PrintMessage(HUD_PRINTTALK, "[scp914] Error during change!")
+        ErrorNoHaltWithStack(sql.LastError())
+    end
+    hook.Run("LuctusSCP914ConfigEdited",ply,inputEnt,outputEnt,wheelnum,rowID)
 end)
 
 net.Receive("luctus_scp914_save", function(len, ply)
-  if not ply:IsValid() or not ply:IsAdmin() then return end
-  local inputEnt = net.ReadString()
-  local outputEnt = net.ReadString()
-  local wheeltype = net.ReadInt(8)
-  local res = sql.Query("INSERT INTO luctus_scp914(input,output,wheeltype) VALUES("..sql.SQLStr(inputEnt)..","..sql.SQLStr(outputEnt)..","..wheeltype..")")
-  if res ~= false then
-    print("[luctus_scp914] Successfully updated list!")
-    print("[luctus_scp914] New entry: "..inputEnt.." / "..outputEnt.." / "..wheeltype)
-    ply:PrintMessage(HUD_PRINTTALK, "[scp914] Added successfully!")
-  else
-    ply:PrintMessage(HUD_PRINTTALK, "[scp914] Error during add, please contact an admin!")
-    print("[luctus_scp914] Error during update list! :")
-    print(sql.LastError())
-  end
+    if not ply:IsValid() or not ply:IsAdmin() then return end
+    local inputEnt = net.ReadString()
+    local outputEnt = net.ReadString()
+    local wheelnum = net.ReadInt(8)
+    local res = sql.Query("INSERT INTO luctus_scp914(input,output,wheelnum,map) VALUES("..sql.SQLStr(inputEnt)..","..sql.SQLStr(outputEnt)..","..wheelnum..", "..sql.SQLStr(game.GetMap())..")")
+    if res ~= false then
+        print("[luctus_scp914] Successfully updated list!")
+        print("[luctus_scp914] New entry: ",inputEnt," -> ",outputEnt," / ",wheelnum)
+        ply:PrintMessage(HUD_PRINTTALK, "[scp914] Added successfully!")
+    else
+        ply:PrintMessage(HUD_PRINTTALK, "[scp914] Error during add!")
+        ErrorNoHaltWithStack(sql.LastError())
+    end
+    hook.Run("LuctusSCP914ConfigAdded",ply,inputEnt,outputEnt,wheelnum)
 end)
 
+LUCTUS_SCP914_CURWHEEL = 0
 
-scp914WheelCounter = scp914WheelCounter or 1
-scp914WheelInitialized = false
+if game.GetMap() == "rp_liquidrp_ct_site99" then
 
-local function SetupWheelCounter()
-  scp914WheelCounter = 1
-  scp914WheelInitialized = false
-  
-  print("[luctus_scp914] Successfully loaded wheelcounter!")
+    LUCTUS_SCP914_WORKAROUND_LQRP = false
+    local function SetupWheelCounter()
+        LUCTUS_SCP914_CURWHEEL = 0
+        LUCTUS_SCP914_WORKAROUND_LQRP = false
+        print("[luctus_scp914] Successfully loaded wheelcounter!")
+    end
+
+    hook.Add("InitPostEntity", "luctus_scp914_wheelreset", SetupWheelCounter)
+    hook.Add("PostCleanupMap", "luctus_scp914_wheelreset", SetupWheelCounter)
+    
+    hook.Add("SCP914_OnWheelPressed", "luctus_scp914", function()
+        --Stupid wheel doesn't turn on first press
+        if LUCTUS_SCP914_WORKAROUND_LQRP == false then
+            LUCTUS_SCP914_WORKAROUND_LQRP = true
+            return
+        end
+        LUCTUS_SCP914_CURWHEEL = (LUCTUS_SCP914_CURWHEEL + 1)%5
+    end)
+    
+    hook.Add("SCP914_OnTeleport", "luctus_scp914", function()
+        --I hope this still works, 20240908
+        local activator, caller = ACTIVATOR, CALLER
+        if not activator or not IsValid(activator) or not activator.GetClass then return end
+        local entClass = activator:GetClass()
+        if activator.GetWeaponClass then
+            entClass = activator:GetWeaponClass()
+        end
+        local outClass = LuctusSCP914GetOutput(entClass,LUCTUS_SCP914_CURWHEEL,game.GetMap())
+        hook.Run("LuctusSCP914Teleported",entClass,outClass,LUCTUS_SCP914_CURWHEEL,game.GetMap())
+        if not outClass then return end
+        print("[luctus_scp914] Transforming ",entClass,"to",outClass)
+        local newPos = activator:GetPos()
+        activator:Remove()
+        local newEnt = ents.Create(outClass)
+        newEnt:SetPos(newPos)
+        newEnt:Spawn()
+    end)
+    
 end
 
-hook.Add( "InitPostEntity", "SCP914_SetupWheelCounter", SetupWheelCounter )
-hook.Add( "PostCleanupMap", "SCP914_SetupWheelCounter", SetupWheelCounter )
-hook.Add( "SCP914_OnWheelPressed", "TestTeleportHook", function()
-  --Stupid wheel doesn't turn on first press
-  if scp914WheelInitialized == false then
-    scp914WheelInitialized = true
-    return
-  end
-  scp914WheelCounter = (scp914WheelCounter + 1)
-  if scp914WheelCounter >= 6 then
-    scp914WheelCounter = 1
-  end
-  print("[luctus_scp914] Wheel has been set to "..scp914WheelCounter)
-end)
-hook.Add( "SCP914_OnTeleport", "TestTeleportHook", function()
-	local activator, caller = ACTIVATOR, CALLER
-	--print( activator, caller )--print("[luctus_scp914] Teleporting...")
-  if(activator and IsValid(activator) and activator:GetClass())then
-    local entClass = activator:GetClass()
-    if (activator.GetWeaponClass) then
-      entClass = activator:GetWeaponClass()
-    end
-    --activator:GetClass()
-    local res = sql.QueryValue("SELECT output FROM luctus_scp914 WHERE input = "..sql.SQLStr(entClass).." AND wheeltype = "..scp914WheelCounter)
-    if res == false then
-      print("[luctus_scp914] ERROR DURING TELEPORT SQL SELECT!")
-      print(sql.LastError())
-      return
-    end
-    if type(res) ~= "string" then return end
-    print("[luctus_scp914] Transforming "..entClass.." to "..res)
-    local destPos = activator:GetPos()
-    activator:Remove()
-    local newEnt = ents.Create(res)
-    newEnt:SetPos(destPos)
-    newEnt:Spawn()
-  end
-end)
+if game.GetMap() == "rp_site65" then
+
+    local site65EndChamber = {Vector("-8493.049805 422.969055 -10881.994141"),Vector("-8555.094727 295.926361 -11017.968750")}
+
+    --hook.Add("SCP-914.Activated","a",function() end)
+    hook.Add("SCP-914.DialChanged","luctus_scp914",function()
+        LUCTUS_SCP914_CURWHEEL = (LUCTUS_SCP914_CURWHEEL+1)%5
+    end)
+    hook.Add("SCP-914.PostEntTeleport","luctus_scp914",function()
+        local tents = ents.FindInBox(site65EndChamber[1],site65EndChamber[2])
+        for k,cent in ipairs(tents) do
+            local entClass = cent:GetClass()
+            if cent.GetWeaponClass then
+                entClass = cent:GetWeaponClass()
+            end
+            local outClass = LuctusSCP914GetOutput(entClass,LUCTUS_SCP914_CURWHEEL,game.GetMap())
+            hook.Run("LuctusSCP914Teleported",entClass,outClass,LUCTUS_SCP914_CURWHEEL,game.GetMap())
+            if not outClass then return end
+            print("[luctus_scp914] Transforming ",entClass,"to",outClass)
+            local newPos = cent:GetPos()
+            cent:Remove()
+            local newEnt = ents.Create(outClass)
+            newEnt:SetPos(newPos)
+            newEnt:Spawn()
+        end
+    end)
+
+end
+
+print("[luctus_scp914] sv loaded")
